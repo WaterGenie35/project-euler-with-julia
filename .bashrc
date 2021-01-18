148b07385da49f35bc070a22eb45b482b727e141
Thirdwater <irinthirdwater@gmail.com>
2021-01-18 14:50:40 +0000

    Group common methods to modules

---
+++ .bashrc
@@ -0,0 +1,1 @@
+export JULIA_LOAD_PATH="/home/thirdwater/projects/project-euler-with-julia/modules:$JULIA_LOAD_PATH"
---
+++ modules/Prime.jl
@@ -0,0 +1,52 @@
+module Prime
+
+export isprime, nthprime, primefactors
+
+function isprime(n::Int)
+    if n <= 3
+        return n >= 2
+    end
+    if (n % 2) == 0
+        return false
+    end
+    factor = 3
+    while (factor ^ 2) <= n
+        if (n % factor) == 0
+            return false
+        end
+        factor += 2
+    end
+    return true
+end
+
+function nthprime(n::Int)
+    if n <= 2
+        return n + 1
+    end
+    index = 3
+    candidate = 5
+    while index < n
+        candidate += 2
+        if isprime(candidate)
+            index += 1
+        end
+    end
+    return candidate
+end
+
+function primefactors(n::Int)
+    factors = Int[]
+    if (n % 2) == 0
+        push!(factors, 2)
+    end
+    factor = 3
+    while (factor ^ 2) <= n
+        if (n % factor) == 0 && isprime(factor)
+            push!(factors, factor)
+        end
+        factor += 2
+    end
+    return factors
+end
+
+end
